c3400f6128a85bccd889d845f65cf1d62df7a79b
yooho husky <yoohohusky@yoohodeMacBook-Pro.local>
2019-10-09 11:09:16 +0800

    测试marven

--- ZeroStart/app/src/main/java/com/example/yooho/zerostart/ui/activity/PaletteActivity.java
+++ ZeroStart/app/src/main/java/com/example/yooho/zerostart/ui/activity/PaletteActivity.java
@@ -3,7 +3,6 @@ package com.example.yooho.zerostart.ui.activity;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.util.Log;
